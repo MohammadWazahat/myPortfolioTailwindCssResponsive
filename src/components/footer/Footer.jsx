dc89c08cbf004a30a39acf56ec1d75ea85c52fe3
MohammadWazahat <muhammadwazahatrza092@gmail.com>
2024-06-03 18:18:17 +0530

    Made Header and Footer Responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,14 +3,12 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div >
+    <div>
       <section>
-        <div class="flex  justify-center my-3">
-          <div class="mx-1">© Copyright 2024 -</div>
-          <div class="mx-1">
-             Developed by Mohammad Wazahat Ali Rza.
-          </div>
-          <div class="mx-1"> All right reserved.</div>
+        <div class="flex flex-col sm:flex-row sm:justify-center sm:my-3">
+          <div class="sm:mx-1 flex justify-center my-2">© Copyright 2024 -</div>
+          <div class="sm:mx-1 flex justify-center my-2">Developed by Mohammad Wazahat Ali Rza.</div>
+          <div class="sm:mx-1 flex justify-center my-2"> All right reserved.</div>
         </div>
       </section>
     </div>
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -7,33 +7,33 @@ const Navbar = () => {
     <div>
       <section>
         <header>
-          <div className="flex mx-20 h-24 ">
-            <div className="basis-1/3 text-3xl my-auto">
+          <div className="flex flex-col py-10 mx-20  sm:flex-row sm:py-3 ">
+            <div className="py-5 sm:basis-1/3 text-3xl sm:my-auto">
               <span className="alphaColor">M</span>ohammad
               <span className="alphaColor"> W</span>azahat
             </div>
-            <div className="basis-2/3 my-auto text-lg">
-              <ul className="flex justify-end ">
-                <li className="mx-3 hover:text-green-300">
+            <div className="py-5 sm:basis-2/3 sm:my-auto text-lg ">
+              <ul className="flex flex-col mx-24 sm:mx-10 sm:flex-row sm:justify-end ">
+                <li className="my-2 sm:mx-3 hover:text-green-300">
                   <NavLink to="/home">Home</NavLink>
                 </li>
-                <li className="mx-3 hover:text-green-300">
+                <li className="my-2 sm:mx-3 hover:text-green-300">
                   <NavLink to="/myResume">My Resume</NavLink>
                 </li>
-                <li className="mx-3 hover:text-green-300">
+                <li className="my-2 sm:mx-3 hover:text-green-300">
                   <NavLink to="/myProjects">My Projects</NavLink>
                 </li>
-                <li className="mx-3 hover:text-green-300">
+                <li className="my-2 sm:mx-3 hover:text-green-300">
                   <NavLink to="/aboutMe">About Me</NavLink>
                 </li>
-                <li className="mx-3 hover:text-green-300">
+                <li className="my-2 sm:mx-3 hover:text-green-300">
                   <NavLink to="/contactMe">Contact Me</NavLink>
                 </li>
               </ul>
             </div>
           </div>
           <div>
-            <hr className="horizon border hover:border-green-500 mx-20 " />
+            <hr className="border hover:border-green-500 mx-5 sm:mx-20 " />
           </div>
         </header>
       </section>
