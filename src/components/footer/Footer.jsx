d79b469a59ae87452a4a8143bce10d23ce0d2629
MohammadWazahat <muhammadwazahatrza092@gmail.com>
2024-08-03 19:12:57 +0530

    footer made responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,21 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div>
-      <section>
-        <div className="flex flex-col sm:flex-row sm:justify-center sm:my-3">
-          <div className="sm:mx-1 flex justify-center my-2">© Copyright 2024 -</div>
-          <div className="sm:mx-1 flex justify-center my-2">Developed by Mohammad Wazahat Ali Rza.</div>
-          <div className="sm:mx-1 flex justify-center my-2"> All right reserved.</div>
+      <section className="mt-24">
+        <div>
+          <hr className="border hover:border-green-500 " />
+        </div>
+        <div className="flex flex-col mt-8 mb-4 md:flex-row md:justify-center md:my-3">
+          <div className="md:mx-1 flex justify-center my-2">
+            © Copyright 2024 -
+          </div>
+          <div className="md:mx-1 flex justify-center my-2">
+            Developed by Mohammad Wazahat Ali Rza.
+          </div>
+          <div className="md:mx-1 flex justify-center my-2">
+            {" "}
+            All right reserved.
+          </div>
         </div>
       </section>
     </div>
