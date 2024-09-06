026c490fe77196493d6dedc7d3bf8bba4bcfedf6
MohammadWazahat <muhammadwazahatrza092@gmail.com>
2024-09-06 18:41:29 +0530

    some small code edited

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,32 +3,38 @@ import "./Footer.css";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
       <section className="abc mt-24 ">
         <div className="flex justify-center items-center gap-12 m-2 p-2">
           <div>
-            <FaLinkedin
-              className="h-6 w-6 "
-              onMouseOver={({ target }) => (target.style.color = "green")}
-              onMouseOut={({ target }) => (target.style.color = "white")}
-            />
+            <Link to="https://www.linkedin.com/in/mohammadwazahatrza092">
+              <FaLinkedin
+                className="h-6 w-6 "
+                onMouseOver={({ target }) => (target.style.color = "green")}
+                onMouseOut={({ target }) => (target.style.color = "white")}
+              />
+            </Link>
           </div>
           <div>
-            <FaGithub
-             className="h-6 w-6 "
-              onMouseOver={({ target }) => (target.style.color = "green")}
-              onMouseOut={({ target }) => (target.style.color = "white")}
-            />
+            <Link to="https://github.com/MohammadWazahat">
+              <FaGithub
+                className="h-6 w-6 "
+                onMouseOver={({ target }) => (target.style.color = "green")}
+                onMouseOut={({ target }) => (target.style.color = "white")}
+              />
+            </Link>
           </div>
           <div>
-            <SiGmail
-              className="h-6 w-6 "
-              onMouseOver={({ target }) => (target.style.color = "green")}
-              onMouseOut={({ target }) => (target.style.color = "white")}
-            />
+            <Link to="https://mail.google.com/">
+              <SiGmail
+                className="h-6 w-6 "
+                onMouseOver={({ target }) => (target.style.color = "green")}
+                onMouseOut={({ target }) => (target.style.color = "white")}
+              />
+            </Link>
           </div>
         </div>
       </section>
@@ -39,15 +45,14 @@ const Footer = () => {
         <div className="flex flex-col mt-8 mb-4 md:flex-row md:justify-center md:my-3">
           <div className="md:mx-2 flex justify-center my-2">
             © Copyright 2024
-            
           </div>
-         
+
           <div className="md:mx-2 flex justify-center my-2">
-          <span className="mx-2">|</span>
+            <span className="mx-2">|</span>
             Developed by Mohammad Wazahat Ali Rza.
             <span className="mx-2 ">|</span>
           </div>
-         
+
           <div className="md:mx-2 flex justify-center my-2">
             All right reserved.
           </div>
